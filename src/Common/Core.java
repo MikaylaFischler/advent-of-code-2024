17e173cb326391a02f2910e3673bfdc864c75037
Mikayla Fischler <mikayla@ky8.io>
2024-12-02 23:27:05 -0500

    core and day 1

---
+++ src/Common/Core.java
@@ -0,0 +1,50 @@
+package Common;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+
+public class Core {
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[0;30m";
+    public static final String RED = "\u001B[0;31m";
+    public static final String YELLOW = "\u001B[0;33m";
+    public static final String GREEN = "\u001B[0;32m";
+    public static final String BLUE = "\u001B[0;34m";
+    public static final String MAGENTA = "\u001B[0;35m";
+    public static final String CYAN = "\u001B[0;36m";
+    public static final String WHITE = "\u001B[0;37m";
+
+//    #define RESET_BG	"\x1B[49m"
+//    #define BLACK_BG	"\x1B[40m"
+//    #define RED_BG		"\x1B[41m"
+//    #define YELLOW_BG	"\x1B[43m"
+//    #define GREEN_BG	"\x1B[42m"
+//    #define BLUE_BG		"\x1B[44m"
+//    #define MAGENTA_BG	"\x1B[45m"
+//    #define CYAN_BG		"\x1B[46m"
+//            #define WHITE_BG	"\x1B[47m"
+//
+    public static final String B_BLACK = "\u001B[1;30m";
+    public static final String B_RED = "\u001B[1;31m";
+    public static final String B_YELLOW = "\u001B[1;33m";
+    public static final String B_GREEN = "\u001B[1;32m";
+    public static final String B_BLUE = "\u001B[1;34m";
+    public static final String B_MAGENTA = "\u001B[1;35m";
+    public static final String B_CYAN = "\u001B[1;36m";
+    public static final String B_WHITE = "\u001B[1;37m";
+
+    public static Scanner readInput(String dir) {
+        String path ="src/".concat(dir.concat("/input.txt"));
+
+        try {
+            return new Scanner(new File(path));
+        } catch (FileNotFoundException ex) {
+            System.out.println(RED + "missing input" + RESET);
+            System.exit(-1);
+        }
+
+        return null;
+    }
+}
---
+++ src/Common/Day.java
@@ -0,0 +1,48 @@
+package Common;
+
+import java.util.Scanner;
+
+public class Day<Part1, Part2> {
+    private final int day;
+    private final String part1Title;
+    private final String part2Title;
+    private Part1 part1Answer;
+    private Part2 part2Answer;
+    private long start;
+
+    public Day(int thisDay, String answer1Title, String answer2Title) {
+        day = thisDay;
+        part1Title = answer1Title;
+        part2Title = answer2Title;
+    }
+
+    public Scanner start() {
+        System.out.println(Core.WHITE + "x------------------------------------------------x");
+        System.out.printf(Core.WHITE + "|           " + Core.BLUE + "2024 " + Core.B_BLUE + "advent of code" + Core.BLUE + " day %02d" + Core.WHITE + "           |\n", day);
+        System.out.println(Core.WHITE + "x------------------------------------------------x\n" + Core.RESET);
+
+        Scanner input = Core.readInput("Day" + day);
+
+        System.out.println(Core.YELLOW + "starting..." + Core.RESET);
+
+        start = System.nanoTime();
+
+        return input;
+    }
+
+    public void recordPart1(Part1 answer) {
+        part1Answer = answer;
+    }
+
+    public void recordPart2(Part2 answer) {
+        part2Answer = answer;
+    }
+
+    public void complete() {
+        System.out.println(Core.GREEN + "done.\n" + Core.RESET);
+
+        System.out.printf(Core.B_WHITE + "total time taken" + Core.WHITE + ": " + Core.RED + "%f" + Core.WHITE + " ms\n", (System.nanoTime() - start) / 1_000_000f);
+        System.out.println(Core.B_RED + "[" + Core.MAGENTA + "part 1" + Core.B_RED + "] " + Core.B_WHITE + part1Title + Core.WHITE + ": " + Core.CYAN + part1Answer);
+        System.out.println(Core.B_RED + "[" + Core.MAGENTA + "part 2" + Core.B_RED + "] " + Core.B_WHITE + part2Title + Core.WHITE + ": " + Core.CYAN + part2Answer + Core.RESET);
+    }
+}
